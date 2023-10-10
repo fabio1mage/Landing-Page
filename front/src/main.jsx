94561896e8ef7811b3a4d5c49c0fc729a04f2a48
Fabio <fridow8@gmail.com>
2023-10-09 21:50:28 -0300

    adding one url

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from './components/Home';
 import PrivacyPolicies from './components/PrivacyPolicies';
 import "./index.scss";
@@ -7,7 +7,8 @@ import "./index.scss";
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
-        <Route element={<Home/>} path='/' exact/>
+        <Route element={<Navigate to='/one'/>} path='/'/>
+        <Route element={<Home/>} path='/one' exact/>
         <Route element={<PrivacyPolicies/>} path='/privacy_policies'/>
       </Routes>
     </BrowserRouter>,
